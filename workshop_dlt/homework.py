7ccc42222b36c810a4f774a1ac8dd479886c0ef0
Leonard Isaac <leoimewore@yahoo.com>
2025-02-14 02:44:48 +0000

    workshop update

---
+++ workshop_dlt/homework.py
@@ -0,0 +1,41 @@
+import dlt
+import os
+import json
+from dlt.sources.helpers.rest_client import RESTClient
+from dlt.sources.helpers.rest_client.paginators import PageNumberPaginator
+
+@dlt.resource(name="rides", write_disposition="replace")
+def ny_taxi():
+    client = RESTClient(
+        base_url="https://us-central1-dlthub-analytics.cloudfunctions.net",
+        paginator=PageNumberPaginator(
+            base_page=1,
+            total_path=None
+        )
+    )
+
+    for page in client.paginate("data_engineering_zoomcamp_api"):
+        yield page
+
+
+pipeline = dlt.pipeline(
+    pipeline_name="ny_taxi_pipeline",
+    destination="duckdb",
+    dataset_name="ny_taxi_data"
+)
+
+load_info = pipeline.run(ny_taxi)
+print(load_info)
+
+import duckdb
+from google.colab import data_table
+data_table.enable_dataframe_formatter()
+
+conn = duckdb.connect(f"{pipeline.pipeline_name}.duckdb")
+
+conn.sql(f"SET search_path ='{pipeline.dataset_name}'")
+conn.sql(f"DESCRIBE").df()
+# conn.execute("SHOW TABLES").fetchall()
+# conn.execute("SELECT COUNT(*) FROM rides")
+
+conn.execute("SELECT AVG(date_diff('minute', trip_pickup_date_time, trip_dropoff_date_time)) FROM rides").df()
