d74d647dcaf5eb1f62d8c8096f47c2cedac5ca6e
Leonard Isaac <dicapino@gmail.com>
2025-02-16 15:13:15 +0000

    update analytics

--- week_4_analytics/taxi_projects/models/core/dim_zones.sql
+++ week_4_analytics/taxi_projects/models/core/dim_zones.sql
@@ -1,6 +1,6 @@
 select 
-locationid, 
-borough, 
-zone, 
-replace(service_zone, 'Boro', 'Green') as service_zone
+    locationid, 
+    borough, 
+    zone, 
+    replace(service_zone, 'Boro', 'Green') as service_zone
 from {{ ref('taxi_zone_lookup') }}
